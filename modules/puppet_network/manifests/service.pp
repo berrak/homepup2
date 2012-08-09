ab4e9e98ab3b6cfbe343558c939979af8ed60368
berrak <bkronmailbox-git@yahoo.se>
2012-08-09 17:44:30 +0200

    Fix: Can't use array of commands to exec, try &&

--- modules/puppet_network/manifests/service.pp
+++ modules/puppet_network/manifests/service.pp
@@ -4,7 +4,7 @@
 class puppet_network::service {
 
   exec { "network_restart":
-          command => ["/etc/init.d/networking stop", "/etc/init.d/networking start"],
+          command => "/etc/init.d/networking stop && /etc/init.d/networking start",
              path => "/bin:/usr/bin:/sbin:/usr/sbin",
       refreshonly => true,
 		  require => File["/etc/network/interfaces"],
