00c28e0d4c8a6f2e8969108ea35d65d5f4a13a6c
berrak <bkronmailbox-git@yahoo.se>
2012-08-13 14:39:14 +0200

    Use templates with data from params.pp

--- modules/puppet_iptables/manifests/params.pp
+++ modules/puppet_iptables/manifests/params.pp
@@ -5,5 +5,28 @@
 class puppet_iptables::params {
 
     $mygateway_hostname = 'gondor'
+    
+    # desktops
+    
+    $net_int = '192.168.0.0/24'
+    $if_int = 'eth0'
+    
+    $myhostaddr = $::ipaddress_eth0
+    $ntphostaddr = '192.168.0.1'
+    
+    # net printers
+    $ntpprn_hp3015_addr = '192.168.0.30'
+    
+    # special addresses
+    $mdnsmulticastaddr = '224.0.0.251'
+    $puppetmasterhostaddr = '192.168.0.24'
+
+
+    # gateway (use also desktop variables '$net_int' and '$if_int' and printers ip)
+    $gwhostaddr = '192.168.0.1'
+    
+    $net_ext = '192.168.1.0/24'
+    $gwhostextaddr = '192.168.1.254'
+    $if_ext = 'eth1'
 
 }
