ef6e040346680cd39b9ddf4d55f0ae734ef6e94a
berrak <bkronmailbox-git@yahoo.se>
2012-08-13 15:03:16 +0200

    Renamed module for clarity.

--- modules/admin_aptconf/manifests/init.pp
+++ modules/admin_aptconf/manifests/init.pp
@@ -1,10 +1,10 @@
 ##
 ## This class configures APT
 ##
-class admin_apt {
+class admin_aptconf {
 
 	file { "/etc/apt/sources.list":
-		source => "puppet:///modules/admin_apt/sources.list",
+		source => "puppet:///modules/admin_aptconf/sources.list",
 		 owner => "root",
 		 group => "root",
 		  mode => '0644',
@@ -14,7 +14,7 @@ class admin_apt {
 	## and to be more verbose when using aptitude.
 	
 	file { "/etc/apt/apt.conf":
-		source => "puppet:///modules/admin_apt/apt.conf",
+		source => "puppet:///modules/admin_aptconf/apt.conf",
 		 owner => 'root',
 		 group => 'root',
 		  mode => '0644',
@@ -24,7 +24,7 @@ class admin_apt {
 	## Configures daily updates/downloads but NOT auto upgrade
 	
 	file { "/etc/apt/apt.conf.d/02periodic":
-		source => "puppet:///modules/admin_apt/02periodic",
+		source => "puppet:///modules/admin_aptconf/02periodic",
 		 owner => 'root',
 		 group => 'root',
 		  mode => '0644',
@@ -34,7 +34,7 @@ class admin_apt {
 	## Always use aptitude safe-upgrade, and call tripwire if it exists
 
 		file { "/root/bin/upgrade":
-			source => "puppet:///modules/admin_apt/aptitude.twupgrade",
+			source => "puppet:///modules/admin_aptconf/aptitude.twupgrade",
 			 owner => 'root',
 			 group => 'root',
 			  mode => '0700',
