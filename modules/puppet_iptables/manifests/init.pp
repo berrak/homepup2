f53874ed057958e25055c96f249c6615e0bce5fc
berrak <bkronmailbox-git@yahoo.se>
2012-08-13 13:23:14 +0200

    Added inherit class to solve resource dependency.

--- modules/puppet_iptables/manifests/init.pp
+++ modules/puppet_iptables/manifests/init.pp
@@ -1,7 +1,7 @@
 ##
 ## Class to manage a set of iptables firewall scripts 
 ##
-class puppet_iptables {
+class puppet_iptables inherit admin_home {
 
     include puppet_iptables::install, puppet_iptables::service, puppet_iptables::config, puppet_iptables::params
 
