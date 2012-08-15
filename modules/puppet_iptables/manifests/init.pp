6d43cfbfb9483dd0e3dd43ba8674c73294e6e5b0
berrak <bkronmailbox-git@yahoo.se>
2012-08-15 13:16:18 +0200

    Fix: iptables inherits one resource in the renamed root_home.

--- modules/puppet_iptables/manifests/init.pp
+++ modules/puppet_iptables/manifests/init.pp
@@ -1,7 +1,7 @@
 ##
 ## Class to manage a set of iptables firewall scripts 
 ##
-class puppet_iptables inherits admin_home {
+class puppet_iptables inherits root_home {
 
     include puppet_iptables::install, puppet_iptables::config, puppet_iptables::params
 
