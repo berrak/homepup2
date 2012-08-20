e097a9c1e9430a5b1693ff0409df3f1a4bfa724d
berrak <bkronmailbox-git@yahoo.se>
2012-08-20 11:55:05 +0200

    Fix: Avoid using any identical (sub)string variables as P't $server variable.

--- modules/puppet_postfix/manifests/params.pp
+++ modules/puppet_postfix/manifests/params.pp
@@ -6,8 +6,8 @@ class puppet_postfix::params {
     # The file location were all preseed files are stored.
     # In this case the '{server|satellite}.postfix.preseed' file.
     
-    $server.preseedfilepath = "/etc/puppet/files/server.postfix.preseed"
-    $satellite.preseedfilepath = "/etc/puppet/files/satellite.postfix.preseed"
+    $server_preseedfilepath = "/etc/puppet/files/server.postfix.preseed"
+    $satellite_preseedfilepath = "/etc/puppet/files/satellite.postfix.preseed"
 
 
 }
