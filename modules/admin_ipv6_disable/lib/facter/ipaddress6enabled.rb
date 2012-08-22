71dfb91af5a8078713d88c4bdfe14f13fdec0a85
berrak <bkronmailbox-git@yahoo.se>
2012-08-22 12:32:17 +0200

    Testing new facts.

--- modules/admin_ipv6_disable/lib/facter/ipaddress6enabled.rb
+++ modules/admin_ipv6_disable/lib/facter/ipaddress6enabled.rb
@@ -6,21 +6,16 @@
 # Resolution:
 # Test if Linux kernel exports the ipv6 variables to user space.
 #
-def get_ipv6_label(output)
-  ip = nil
-
-  output.scan(/ipv6/).each do |match|
-    match = match.first
-  end
-  
-  ip = match
-end
-
 Facter.add(:ipaddress6enabled) do
   confine :kernel => :linux
   setcode do
     output = Facter::Util::Resolution.exec('/bin/ls /proc/sys/net')
-    get_ipv6_label(output)
+    ip = nil
+    
+    output.scan(/ipv6/).each do |str|
+      ip = str.to_s
+    end
+    
   end
 end
 
