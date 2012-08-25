6b071f93ea1f8d268f52e3f2d378b5ee283179d6
berrak <bkronmailbox-git@yahoo.se>
2012-08-25 09:34:00 +0200

    Rewrote the custom fact 'is_ipv6host' to return either false or true.

--- modules/admin_ipv6_disable/manifests/config.pp
+++ modules/admin_ipv6_disable/manifests/config.pp
@@ -1,9 +1,9 @@
 ##
-## This class disable (or enable again) ipv6 in the kernel. After a system
-## reboot confirm with "dmesg | grep -i ipv6" which should give:
+## This class disable ipv6 in the kernel. After a reboot
+## confirm with "dmesg | grep -i ipv6" which should give:
 ## "Ipv6: Loaded, but administratively disabled, ..."
-## or 'ls -l /proc/sys/net | grep ipv6'
-##
+## or:
+## "ls -l /proc/sys/net | grep ipv6"
 ##
 class admin_ipv6_disable::config {
 
