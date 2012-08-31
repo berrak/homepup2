cccdbcdfe475af34f598583aac4509475eb255ef
berrak <bkronmailbox-git@yahoo.se>
2012-08-31 22:25:03 +0200

    Fix: Dependency in define missing. Include init.pp.

--- modules/puppet_mutt/manifests/install.pp
+++ modules/puppet_mutt/manifests/install.pp
@@ -3,6 +3,8 @@
 ##
 define puppet_mutt::install {
 
+    include puppet_mutt
+
     file { "/home/${name}/.muttrc" : 
         source => "puppet:///modules/puppet_mutt/muttrc",
         require => Package["mutt"],
