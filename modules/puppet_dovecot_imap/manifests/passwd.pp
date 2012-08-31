3b153da799a1717a7d346689cb89b74903254294
berrak <bkronmailbox-git@yahoo.se>
2012-08-31 15:35:19 +0200

    Fix: passwd file never created, add file resource to create initial file.

--- modules/puppet_dovecot_imap/manifests/passwd.pp
+++ modules/puppet_dovecot_imap/manifests/passwd.pp
@@ -6,10 +6,16 @@ class puppet_dovecot_imap::passwd {
 
     include puppet_utils::append_if_no_such_line
 
-    puppet_utils::append_if_no_such_line { "create_imap_user " :
+    file { "/etc/dovecot/imap.passwd":
+        ensure => present,
+		 owner => 'root',
+		 group => 'root',
+	}    
+
+    puppet_utils::append_if_no_such_line { "create_imap_user" :
                         
                         file => '/etc/dovecot/imap.passwd',
-                        line => 'bekr:{PLAIN}pass'
+                        line => 'bekr:{PLAIN}pass',
     }
 
 }
