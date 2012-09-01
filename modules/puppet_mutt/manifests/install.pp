9888f179901a084070f55d0a19553ab22f47d19e
berrak <bkronmailbox-git@yahoo.se>
2012-09-01 16:03:40 +0200

    Added modified muttrc file for Maildir format.

--- modules/puppet_mutt/manifests/install.pp
+++ modules/puppet_mutt/manifests/install.pp
@@ -1,12 +1,28 @@
 ##
-## Define to manage mutt Mail User Agent (mua).
+## 'Define' to install mutt resource file.
+##
+##  Sample use:
+##  	puppet_mutt::install { 'bekr': }
+##      puppet_mutt::install { 'root': }
 ##
 define puppet_mutt::install {
 
-    file { "/home/${name}/.muttrc" : 
-        source => "puppet:///modules/puppet_mutt/muttrc",
-         owner => $name,
-         group => $name,
+    if $name == 'root' {
+    
+        file { "/root/.muttrc" : 
+            source => "puppet:///modules/puppet_mutt/muttrc",
+             owner => 'root',
+             group => 'root',
+        }
+    
+    } else {
+
+        file { "/home/${name}/.muttrc" : 
+            source => "puppet:///modules/puppet_mutt/muttrc",
+             owner => $name,
+             group => $name,
+        }
+        
     }
     
 }
