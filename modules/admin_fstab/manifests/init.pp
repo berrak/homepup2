dfff41db291de97538b7093602252cb624f2b15d
berrak <bkronmailbox-git@yahoo.se>
2012-09-02 15:26:19 +0200

    Initial work to make the admin_fstab more forgiving and robust.

--- modules/admin_fstab/manifests/init.pp
+++ modules/admin_fstab/manifests/init.pp
@@ -1,17 +1,52 @@
 ##
 ## This class manage the system 'fstab' file. Place a host specific file
 ## in the source directory prefixed with output of 'hostname'+"."+"fstab"
+## and a CSV file (e.g. 'carbon.csv') containing the sda1 disk UUID like so:
+##		'uuid, 064b4f90-9c73-49ea-8734-9b62bfd55471'
 ##
 ## Sample use:
 ##    class { admin_fstab : fstabhost => 'carbon' }
 ##
-class admin_fstab ( $fstabhost='' ) {
+class admin_fstab ( $fstabhost='', $source = 'UNSET' ) {
 	
-	if ! ( $fstabhost in [ "carbon", "gondor", "rohan", "mordor" ] ) {
+	$extlookup_datadir = "/etc/puppet/files"
 	
-		fail("FAIL: Could not find ($fstabhost) fstab file on puppetmaster! Please copy it over.")
-	
+    # this is the original data file for each host 
+    $real_source = $source ? {
+        'UNSET' => "puppet:///modules/admin_fstab/${fstabhost}.csv",
+        default => $source,
 	}
+	
+	# this is the UUID data CSV for for host 
+    file { "/etc/puppet/files/${fstabhost}.csv" : 
+         source => $real_source,
+          owner => 'root',
+          group => 'root',
+    }
+	
+	$fstab_uuid_sda1 = extlookup( $fstabhost )
+	
+	notify{"external uuid lookup returns ($fstab_uuid_sda1) for ($fstabhost)" : }
+	
+    # TODO
+	# 1. use $fstabhost to read in host and disk uuid (rrot) from external cv-file.
+	
+	# 2. if host is not found, skip the fstab sourcing below
+	
+	# 3. do not fail but print notify about missing fstab
+	
+	# 4. if host match then compare the host uuid with the named hostuuid fstab:
+	# '064b4f90-9c73-49ea-8734-9b62bfd55471.fstab_uuid_sda1_carbon'
+	
+	
+	
+	#if ! ( $fstabhost in [ "carbon", "gondor", "rohan", "mordor" ] ) {
+	#
+	#	fail("FAIL: Could not find ($fstabhost) fstab file on puppetmaster! Please copy it over.")
+	#
+	#}
+
+    
 
 	file { "/etc/fstab":
 		source => "puppet:///modules/admin_fstab/${fstabhost}.fstab",
