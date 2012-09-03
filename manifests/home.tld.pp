00575aa4ecf431ff5a936a1bb52fc3cf4bec5674
berrak <bkronmailbox-git@yahoo.se>
2012-09-03 11:31:02 +0200

    The gateway host can only have one default gateway definition (on eth1).

--- manifests/home.tld.pp
+++ manifests/home.tld.pp
@@ -86,9 +86,9 @@ node 'gondor.home.tld' inherits basenode {
     class { puppet_iptables::config : role => 'gateway' }
 	
     class { puppet_network::interfaces :
-		iface_zero => 'eth0', gateway_zero => '192.168.0.1', bcstnet_zero => '192.168.0.255',
-		iface_one => 'eth1', gateway_one => '192.168.1.1', bcstnet_one => '192.168.1.255',
-		addfirewall => 'true' }
+		iface_zero => 'eth0', bcstnet_zero => '192.168.0.255',
+		iface_one => 'eth1', bcstnet_one => '192.168.1.255',
+		gateway_one => '192.168.1.1', addfirewall => 'true' }
 	
 	# lan ntp server provids time services to all lan clients
     class { 'puppet_ntp' : role => 'lanserver', peerntpip => '192.168.0.1' }
