ef9492eedddf674ae2f0237f7ecfed1c30760352
berrak <bkronmailbox-git@yahoo.se>
2012-09-06 19:36:35 +0200

    Initial script for unattended upgrades.

---
+++ modules/admin_aptconf/files/aptitude.unattended_upgrade.sh
@@ -0,0 +1,36 @@
+#!/bin/sh -e
+#
+# /root/bin/aptitude.unattended_upgrade.sh
+#
+###############################################################
+# MANAGED BY PUPPET. DO NOT EDIT. CHANGES WILL BE WIPED OUT.  #
+###############################################################
+#
+# Runs an aptitude unattended (cron) script for security updates.
+#
+# Requires script 'upgrade' (which runs tripwire if available)
+#
+# Initial version without any error checks - todo.
+
+UPGRADE="/root/bin/upgrade"
+MV="/bin/mv"
+
+QUIET="2>&1 /dev/null"
+
+ORIGINALSOURCEDIR="/etc/apt/sources.list.d"
+SHADOWSOURCEDIR="/etc/apt/.sources.list.d"
+
+# move the source snippets to our shadow directory
+
+$MV $ORIGINALSOURCEDIR/* $SHADOWSOURCEDIR
+
+# runs the upgrade with repos that only is in 'source.list'
+
+$UPGRADE $QUIET
+
+# now move source snippets back to its original place
+
+$MV $SHADOWSOURCEDIR/* $ORIGINALSOURCEDIR
+
+exit 0
+
--- modules/admin_aptconf/manifests/init.pp
+++ modules/admin_aptconf/manifests/init.pp
@@ -3,7 +3,8 @@
 ##
 class admin_aptconf {
 
-	# this source list is always executed, eventually unattended	
+	# the source list alone is always included
+	# when cron runs our script 'aptitude.unattended'
 
 	file { "/etc/apt/sources.list":
 		source => "puppet:///modules/admin_aptconf/sources.list",
@@ -13,7 +14,11 @@ class admin_aptconf {
 	}
 
 	# put all other sources in the sources.list.d directory.	
-	# source list snippets may be disabled during security runs. 	
+	# source list snippets is disabled during unattended runs.
+	
+	
+	
+	### repo files
 
 	file { "/etc/apt/sources.list.d/main.list":
 		source => "puppet:///modules/admin_aptconf/main.list",
@@ -21,6 +26,17 @@ class admin_aptconf {
 		 group => "root",
 		  mode => '0644',
 	}
+
+
+	file { "/etc/apt/sources.list.d/updates.list":
+		source => "puppet:///modules/admin_aptconf/updates.list",
+		 owner => "root",
+		 group => "root",
+		  mode => '0644',
+	}
+
+    ### end repo files
+
 
 
 	## Configures APT for recommend/suggests to "false"
@@ -34,23 +50,43 @@ class admin_aptconf {
 
 	}	
 
-	## Configures daily updates/downloads but NOT auto upgrade
+	## Configures daily updates/downloads but do NOT auto upgrade (i.e. do
+	## not use the package unattended-upgrades. Use my 'aptitude.unattended'
 	
 	file { "/etc/apt/apt.conf.d/02periodic":
 		source => "puppet:///modules/admin_aptconf/02periodic",
 		 owner => 'root',
 		 group => 'root',
 		  mode => '0644',
-
 	}
+	
+	# create a bin subdirectory directory only for an unattended cron
+	# upgrade script. Needs this to shuffle source lists when run unattended.
+	
+	file { "/etc/apt/.sources.list.d":
+		ensure => "directory",
+		owner => 'root',
+		group => 'root',
+	}
+	
 	
 	## Always use aptitude safe-upgrade, and call tripwire if it exists
 
 		file { "/root/bin/upgrade":
-			source => "puppet:///modules/admin_aptconf/aptitude.twupgrade",
+			source => "puppet:///modules/admin_aptconf/aptitude.twupgrade.sh",
 			 owner => 'root',
 			 group => 'root',
 			  mode => '0700',
 		}
+		
+	## Always use this script for unattended security updates. This script
+	## will only read 'sources.list' and not any snippets in /sources.list.d
+
+		file { "/root/bin/aptitude.unattended_upgrade":
+			source => "puppet:///modules/admin_aptconf/aptitude.unattended_upgrade.sh",
+			 owner => 'root',
+			 group => 'root',
+			  mode => '0700',
+		}	
 
 }
