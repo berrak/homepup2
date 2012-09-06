5abbf3fef384cb150bf1d6c3cf176dae1e83d997
berrak <bkronmailbox-git@yahoo.se>
2012-09-06 16:08:26 +0200

    Preparation to run unattended apt/cron security updates.

--- modules/admin_aptconf/manifests/init.pp
+++ modules/admin_aptconf/manifests/init.pp
@@ -3,12 +3,25 @@
 ##
 class admin_aptconf {
 
+	# this source list is always executed, eventually unattended	
+
 	file { "/etc/apt/sources.list":
 		source => "puppet:///modules/admin_aptconf/sources.list",
 		 owner => "root",
 		 group => "root",
 		  mode => '0644',
 	}
+
+	# put all other sources in the sources.list.d directory.	
+	# source list snippets may be disabled during security runs. 	
+
+	file { "/etc/apt/sources.list.d/main.list":
+		source => "puppet:///modules/admin_aptconf/main.list",
+		 owner => "root",
+		 group => "root",
+		  mode => '0644',
+	}
+
 
 	## Configures APT for recommend/suggests to "false"
 	## and to be more verbose when using aptitude.
