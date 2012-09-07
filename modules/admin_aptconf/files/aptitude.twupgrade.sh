5c80b2a406fcf823000684e3ab0321202e857e85
berrak <bkronmailbox-git@yahoo.se>
2012-09-07 14:21:19 +0200

    Added some error handling.

--- modules/admin_aptconf/files/aptitude.twupgrade.sh
+++ modules/admin_aptconf/files/aptitude.twupgrade.sh
@@ -6,9 +6,28 @@
 # MANAGED BY PUPPET. DO NOT EDIT. CHANGES WILL BE WIPED OUT.  #
 ###############################################################
 # Console usage: upgrade [option]
-# aptitude safe-upgrade with (possible) tripwire run
-
-# $1 can hold any additional options, but here we receive '-y'
+# runs in sequence: aptitude update, aptitude safe-upgrade
+#                   and tripwire --check (if installed)
+#
+# Additional options can be e.g. '-y'| '--assumes-yes'
+# Default options in $OPT (see below)
+#   --prompt --show-versions --verbose --without-recommends
+#
+# Copyright (C) (2012) K-B.Kronlund <bkronmailbox-copyright@yahoo.se>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
 
 if [ -z $1 ] ; then
     OPT="--prompt --show-versions --verbose --without-recommends"
@@ -22,6 +41,8 @@ if [ $? -eq 0 ] ; then
     /usr/bin/aptitude $OPT safe-upgrade
 fi
 
+
+# tripwire.check is tripwire --check from a safe binary
 if [ -d "/etc/tripwire" ] ; then 
     /root/bin/tripwire.check
 else
--- modules/admin_aptconf/files/aptitude.unattended_upgrade.sh
+++ modules/admin_aptconf/files/aptitude.unattended_upgrade.sh
@@ -1,4 +1,4 @@
-#!/bin/sh -e
+#!/bin/sh
 #
 # /root/bin/upgrade.security
 #
@@ -6,29 +6,60 @@
 # MANAGED BY PUPPET. DO NOT EDIT. CHANGES WILL BE WIPED OUT.  #
 ###############################################################
 #
-# Runs an aptitude unattended (cron) script for security updates.
+# Runs script for security updates (unattended).
 #
 # Requires script 'upgrade' (which runs tripwire if available)
+# and use aptitude, but in principle could be apt-get as well.
 #
-# Initial version without any error checks - todo.
+# Copyright (C) (2012) K-B.Kronlund <bkronmailbox-copyright@yahoo.se>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
 
 UPGRADE="/root/bin/upgrade"
 MV="/bin/mv"
+MKDIR="/bin/mkdir"
 
 ORIGINALSOURCEDIR="/etc/apt/sources.list.d"
 SHADOWSOURCEDIR="/etc/apt/.sources.list.d"
 
+# test if both directories exists, create them if not.
+
+if [ ! -d $ORIGINALSOURCEDIR ] ; then
+    $MKDIR $ORIGINALSOURCEDIR
+fi
+
+if [ ! -d $SHADOWSOURCEDIR ] ; then
+    $MKDIR $SHADOWSOURCEDIR
+fi
+
 # move the source snippets to our shadow directory
+# redirect stderr to /dev/null in case no files found
 
-$MV $ORIGINALSOURCEDIR/* $SHADOWSOURCEDIR
+$MV $ORIGINALSOURCEDIR/* $SHADOWSOURCEDIR 2> /dev/null
+movedfiles=$?
 
-# runs the upgrade with repos that only is in 'source.list'
+# runs the upgrade with repos that only is in 'sources.list'
+# which is security important and 'safe' to run unattended.
 
 $UPGRADE -y
 
-# now move source snippets back to its original place
+# move possible source snippets back to its original place
 
-$MV $SHADOWSOURCEDIR/* $ORIGINALSOURCEDIR
+if [ $movedfiles -eq 0 ] ; then
+    $MV $SHADOWSOURCEDIR/* $ORIGINALSOURCEDIR
+fi
 
 exit 0
 
