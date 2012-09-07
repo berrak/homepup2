e4824a517342dce9ae543d63c9593f0ff305c0a3
berrak <bkronmailbox-git@yahoo.se>
2012-09-07 18:11:58 +0200

    Does not run by run-parts (cron.daily). Try with entry in /etc/cron.d.

--- modules/puppet_tripwire/manifests/cron.pp
+++ modules/puppet_tripwire/manifests/cron.pp
@@ -3,12 +3,22 @@
 ##
 class puppet_tripwire::cron {
 	
-    file { "/etc/cron.daily/tripwire" :
+#    file { "/etc/cron.daily/tripwire" :
+#		source => "puppet:///puppet_tripwire/cron.tripwire",
+#		 owner => 'root',
+#		 group => 'root',
+#		  mode => '0700',
+#    }
+
+
+    file { "/etc/cron.d/tripwire" :
 		source => "puppet:///puppet_tripwire/cron.tripwire",
 		 owner => 'root',
 		 group => 'root',
-		  mode => '0700',
+		  mode => '0644',
     }
+
+
 
 
 }
