3388e8343d8c1c958203c2205f76f7b1c40b6d93
berrak <bkronmailbox-git@yahoo.se>
2012-09-09 10:28:05 +0200

    Fix: use of variable environment (cron). Recoded module.

--- modules/admin_cron/manifests/install.pp
+++ modules/admin_cron/manifests/install.pp
@@ -2,32 +2,34 @@
 # Define (install) a root cron job
 #
 define admin_cron::install (
-		$ensure=present,
 		$command='',
-		$user='root,
 		$hour=undef,
 		$minute=undef,
 		$month=undef,
 		$monthday=undef,
 		$weekday=endef,
-		$environment='PATH=/root/bin:/sbin:/bin',
 		$special=undef,
 ) {
+
+		$myensure = 'present'
+		$mycommand = $command
+		$myuser = 'root'
+		$mypath = 'PATH=/root/bin:/sbin:/bin'
 
     if $command == '' {
 		fail("FAIL: $name is missing the command parameter ($command).")
 	}
 
 	cron { "$name":
-		ensure		=> $ensure,
-		command 	=> "$command",
-  		user 		=> $user,
+		ensure		=> $myensure,
+		command 	=> $mycommand,
+		environment => $mypath,
+  		user 		=> $myuser,
   		minute 		=> $minute,
 		hour		=> $hour,
 		month		=> $month,
 		monthday	=> $monthday,
 		weekday		=> $weekday,
-		environment => $environment,
 		special		=> $special,
 	}
 }
