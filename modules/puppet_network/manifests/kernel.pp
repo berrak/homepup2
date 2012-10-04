5daf1ea813f3f403353b85b9327d9d6136686556
berrak <bkronmailbox-git@yahoo.se>
2012-10-04 10:54:53 +0200

    Modified some network security settings.

--- modules/puppet_network/manifests/kernel.pp
+++ modules/puppet_network/manifests/kernel.pp
@@ -8,6 +8,9 @@ class puppet_network::kernel {
 
     $myhost = $::hostname
 
+
+    # special case for our gateway host (enable forwarding)
+    
     if $myhost == 'gondor' {
 
         file { "/etc/sysctl.conf":
@@ -16,6 +19,14 @@ class puppet_network::kernel {
              group => 'root',
               mode => '0644',
         }
+    
+    } else {
+    
+        file { "/etc/sysctl.conf":
+            source => "puppet:///modules/puppet_network/sysctl.conf.default",
+             owner => 'root',
+             group => 'root',
+              mode => '0644',  
     
     }
 
