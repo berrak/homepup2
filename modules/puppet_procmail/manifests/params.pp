d59b3769e5c9131c944eae14edc81b0d772784eb
berrak <bkronmailbox-git@yahoo.se>
2012-10-10 19:44:20 +0200

    Any local mails can't be matched by hostname. Unmatched default these to .INBOX.

--- modules/puppet_procmail/manifests/params.pp
+++ modules/puppet_procmail/manifests/params.pp
@@ -14,7 +14,9 @@ class puppet_procmail::params {
     
     # list for the dovecot/imap 'subscriptions' folders 
     # (capatilized) to match the folders names in recipes.rc.
+    # Do not include the mail server host here. Local mails
+    # is routed by default to top .INBOX by 'procmail'.
     
-    $hostsubscriptionfolders = [ 'GONDOR', 'CARBON', 'VALHALL', 'MORDOR', 'ROHAN' ]
+    $hostsubscriptionfolders = [ 'GONDOR', 'CARBON', 'VALHALL', 'MORDOR' ]
 
 }
