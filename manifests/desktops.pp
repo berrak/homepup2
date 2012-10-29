a4e92f4de6537a8aad741999b79d7f4167c5c6f0
berrak <bkronmailbox-git@yahoo.se>
2012-10-29 17:37:40 +0100

    Added new host 'shire' to nodes.

--- manifests/desktops.pp
+++ manifests/desktops.pp
@@ -48,7 +48,50 @@ node 'mordor.home.tld' inherits basenode {
 	
 
 }
+#########################################
+## (SHIRE) developer host (desktop)
+#########################################
+node 'shire.home.tld' inherits basenode {
 
+    include puppet_agent
+
+    # assumes that all host lives in the same domain, otherwise specify it as a parameter
+    class { admin_hosts::config :
+        puppetserver_ip => '192.168.0.24', puppetserver_hostname => 'carbon',
+        gateway_ip => '192.168.0.1', gateway_hostname => 'gondor',
+        smtp_ip => '192.168.0.11', smtp_hostname => 'rohan' }
+
+    # Note: requires a copy of hosts 'fstab' file at puppetmaster.
+    class { admin_fstab::config : fstabhost => 'shire' }
+
+    # load desktop firewall script
+    class { puppet_iptables::config : role => 'default.desktop' }
+	 
+    class { puppet_network::interfaces : broadcastnet => '192.168.0.0', defaultgateway => '192.168.0.1' }
+		
+	class { 'puppet_ntp' : role => 'lanclient', peerntpip => $ipaddress }
+	
+    puppet_postfix::install { 'mta' : ensure => installed, install_cyrus_sasl => 'false',
+				mta_type => satellite, smtp_relayhost_ip => '192.168.0.11' }		
+	
+	## users
+	
+    puppet_mutt::install { 'root': mailserver_hostname => 'rohan' }
+	
+    user_bashrc::config { 'bekr' : }
+    puppet_mutt::install { 'bekr': mailserver_hostname => 'rohan' }
+    puppet_devtools::tools { 'bekr' : }
+	
+	# puppet_git::config { 'bekr': codehost => 'mordor' }
+
+	## application bundles
+	
+    admin_bndl::install { 'guiadminapps' : }
+    admin_bndl::install { 'officeapps' : }
+    admin_bndl::install { 'developerapps' : }
+	
+    include puppet_cups
+	
 ###############################
 ## eof
 ###############################
