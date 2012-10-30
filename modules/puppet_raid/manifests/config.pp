64d4b5f41315f3b7d00f0b1e9fd579fd707283e7
berrak <bkronmailbox-git@yahoo.se>
2012-10-30 11:26:54 +0100

    Dbg: conf is not sourced.

--- modules/puppet_raid/manifests/config.pp
+++ modules/puppet_raid/manifests/config.pp
@@ -3,17 +3,23 @@
 ##
 class puppet_raid::config {
 
-    include puppet_raid::params
+    file { '/etc/mdadm/mdadm.conf':
+        source => "puppet:///modules/puppet_raid/mdadm.conf",    
+         owner => 'root',
+         group => 'root',
+       require => Class["puppet_raid::install"],
+    }    
     
-    if $::hostname == puppet_raid::params::raidhostname {
-    
-        file { '/etc/mdadm/mdadm.conf':
-            source => "puppet:///modules/puppet_raid/mdadm.conf",    
-             owner => 'root',
-             group => 'root',
-           require => Class["puppet_raid::install"],
-        }
-        
-    }
+    #include puppet_raid::params
+    #if $::hostname == puppet_raid::params::raidhostname {
+    #
+    #    file { '/etc/mdadm/mdadm.conf':
+    #        source => "puppet:///modules/puppet_raid/mdadm.conf",    
+    #         owner => 'root',
+    #         group => 'root',
+    #       require => Class["puppet_raid::install"],
+    #    }
+    #    
+    #}
 
 }
