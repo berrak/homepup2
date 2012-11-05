b07d9e28fe1392785e8cdc4bc02d434e73470f9d
berrak <bkronmailbox-git@yahoo.se>
2012-11-05 12:38:49 +0100

    Expliciely show 'no_subtree_check' (default) in exports file.

--- modules/puppet_nfs4srv/manifests/params.pp
+++ modules/puppet_nfs4srv/manifests/params.pp
@@ -5,6 +5,6 @@ class puppet_nfs4srv::params {
 
     # export the root 'pseudo filesystem' to internal net
 
-    $export0 = '/mnt/exports  -sec=sys 192.168.0.0/24(rw,fsid=0)'    
+    $export0 = '/mnt/exports  -sec=sys 192.168.0.0/24(rw,fsid=0,no_subtree_check)'    
 
 }
