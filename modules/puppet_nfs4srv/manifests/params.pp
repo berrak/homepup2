2fdf1001bc764a469a2ab651e5723d0dfde8cda2
berrak <bkronmailbox-git@yahoo.se>
2012-11-05 19:26:39 +0100

    Fix: Reverted back the NFSv4 exports to previous working version.

--- modules/puppet_nfs4srv/manifests/params.pp
+++ modules/puppet_nfs4srv/manifests/params.pp
@@ -5,10 +5,6 @@ class puppet_nfs4srv::params {
 
     # export the nfs root 'pseudo filesystem' to internal net
 
-    $export0 = '/exports/nfs -sec=sys,ro,no_subtree_check 192.168.0.0/24(fsid=0)'
-    
-    # export the local nfs/bekr to other hosts were user have an account
-    
-    $export1 = '/exports/nfs/bekr 192.168.0.100/24(rw)'
+    $export0 = '/exports/nfs 192.168.0.0/24(rw,fsid=0,no_subtree_check)'
 
 }
