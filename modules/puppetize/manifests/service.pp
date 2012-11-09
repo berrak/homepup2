f771254d2855dc3a73d0a6baf20974308f421212
berrak <bkronmailbox-git@yahoo.se>
2012-11-09 20:35:28 +0100

    Added some text to script 'puppet.exec' and ensure service is not daemonized.

--- modules/puppetize/manifests/service.pp
+++ modules/puppetize/manifests/service.pp
@@ -25,7 +25,7 @@ class puppetize::service {
               name => "puppet",
             enable => false,
         hasrestart => true,
-            ensure => running,
+            ensure => stopped,
 		   require => Class["puppetize::install"],
     }
     
