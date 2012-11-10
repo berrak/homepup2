2203ad1caf02f1e91e0a5e559601fd14dcb5a690
berrak <bkronmailbox-git@yahoo.se>
2012-11-10 10:58:02 +0100

    Corrected file owner and mode for logcheck files.

--- modules/puppet_logcheck/manifests/config.pp
+++ modules/puppet_logcheck/manifests/config.pp
@@ -8,9 +8,10 @@ class puppet_logcheck::config {
         if $::hostname == $::puppet_logcheck::params::myloghost {
         
             file { '/etc/logcheck/logcheck.conf':
-                owner => 'root',
-                group => 'root',
-                source => "puppet:///modules/puppet_logcheck/logcheck.conf",
+                 source => "puppet:///modules/puppet_logcheck/logcheck.conf",        
+                  owner => 'root',
+                  group => 'logcheck',
+                   mode => '0640',
                 require => Class["puppet_logcheck::install"],
             }
         
@@ -18,19 +19,21 @@ class puppet_logcheck::config {
             # list of logfiles to check
         
             file { '/etc/logcheck/logcheck.logfiles':
-                owner => 'root',
-                group => 'root',
-                source => "puppet:///modules/puppet_logcheck/logcheck.logfiles",
+                 source => "puppet:///modules/puppet_logcheck/logcheck.logfiles",
+                  owner => 'root',
+                  group => 'logcheck',
+                   mode => '0640',
                 require => Class["puppet_logcheck::install"],
             }
             
             
-            # email haeder text
+            # email header text (if enabled in configuration)
         
             file { '/etc/logcheck/header.txt':
-                owner => 'root',
-                group => 'root',
-                source => "puppet:///modules/puppet_logcheck/header.txt",
+                 source => "puppet:///modules/puppet_logcheck/header.txt",  
+                  owner => 'root',
+                  group => 'logcheck',
+                  mode => '0640',
                 require => Class["puppet_logcheck::install"],
             }
         
