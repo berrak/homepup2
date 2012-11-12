b9b5f9f8e06bf3876c35a8def970f86666059d3c
berrak <bkronmailbox-git@yahoo.se>
2012-11-12 11:15:42 +0100

    Changed the default mode to 0700

--- modules/admin_rsyslog/manifests/config.pp
+++ modules/admin_rsyslog/manifests/config.pp
@@ -14,6 +14,7 @@ class admin_rsyslog::config {
     file { '/root/bin/rsyslog.logtest':
         owner => 'root',
         group => 'root',
+         mode => '0700',
         source => "puppet:///modules/admin_rsyslog/rsyslog.logtest",
         require => Class["admin_rsyslog::install"],
     }  
