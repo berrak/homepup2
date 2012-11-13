8e0ea64fa379653587a5af001765054d8d8686fa
berrak <bkronmailbox-git@yahoo.se>
2012-11-13 09:58:03 +0100

    Added configuration for central loghost.

--- modules/admin_rsyslog/manifests/config.pp
+++ modules/admin_rsyslog/manifests/config.pp
@@ -5,42 +5,45 @@ class admin_rsyslog::config {
 
     include admin_rsyslog::params
     
-    if $::hostname == admin_rsyslog::params::myloghost {
+    if $::hostname == $::admin_rsyslog::params::myloghost {
      
-        $sendtohost = '#'
         $srvloadtcp = $::admin_rsyslog::params::logsrvloadtcp
         $srvport = $::admin_rsyslog::params::logsrvportinput
+        $srvremotepath = $::admin_rsyslog::params::logsrvremotepath
         
-        # create a directory for remote logs
+        file { '/etc/rsyslog.conf':
+            content => template( '/admin_rsyslog/rsyslog.conf.loghost.erb' ),
+              owner => 'root',
+              group => 'root',
+            require => Class["admin_rsyslog::install"],
+             notify => Class["admin_rsyslog::service"],
+        }        
         
-        file ( $::admin_rsyslog::params::logsrvremotepath ) {
+        # create a directory for all remote logs
+        
+        file { $srvremotepath :
             ensure => directory,
         	 owner => 'root',
 	 	     group => 'adm',
 		      mode => '0755',
         }
         
-        # TODO: add template or rule to save diffrent hosts to diff files
-        
     
     } else {
     
         $sendtohost = $::admin_rsyslog::params::sendtologhost
-        $srvloadtcp = "#$::admin_rsyslog::params::logsrvloadtcp"
-        $srvport = "#$::admin_rsyslog::params::logsrvportinput"
+        
+        file { '/etc/rsyslog.conf':
+            content => template( '/admin_rsyslog/rsyslog.conf.erb' ),
+              owner => 'root',
+              group => 'root',
+            require => Class["admin_rsyslog::install"],
+             notify => Class["admin_rsyslog::service"],
+        }
     
     }
     
-    file { '/etc/rsyslog.conf':
-        content => template( '/admin_rsyslog/rsyslog.conf.erb' ),
-          owner => 'root',
-          group => 'root',
-        require => Class["admin_rsyslog::install"],
-         notify => Class["admin_rsyslog::service"],
-    }
-    
-    
-    
+    # logger wrapper script to test facilities.priorities at command line 
     
     file { '/root/bin/rsyslog.test':
          source => "puppet:///modules/admin_rsyslog/rsyslog.test",
--- modules/admin_rsyslog/manifests/params.pp
+++ modules/admin_rsyslog/manifests/params.pp
@@ -5,7 +5,7 @@ class admin_rsyslog::params {
 
 
     ## log server (i.e. the receiving log host)
-    
+    ############################################
     $myloghost = 'warp'
     
     $logsrvloadtcp = '$ModLoad imtcp'
@@ -16,8 +16,8 @@ class admin_rsyslog::params {
     
     
     
-    ## client (i.e. sending all rsyslog messages on tcp port 1958)
-    
-    $sendtologhost = '*.*         @@192.168.0.83:1958'
+    ## client config (i.e. sending almost all rsyslog messages to loghost)
+    ######################################################################
+    $sendtologhost = '*.*;auth,authpriv.none;kern.!emerg   @@192.168.0.83:1958'
 
 }
