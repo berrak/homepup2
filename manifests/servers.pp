038b01d477f9fce1a394231448624e6db97635fc
berrak <bkronmailbox-git@yahoo.se>
2012-11-16 18:54:11 +0100

    Moved log host to own sub domain.

--- manifests/servers.pp
+++ manifests/servers.pp
@@ -134,21 +134,21 @@ node 'valhall.sec.home.tld' inherits basenode {
 ##########################################################
 ## (WARP) - Fileserver: Will 'warp' move to sec.home.tld
 ##########################################################
-node 'warp.home.tld' inherits basenode {
+node 'warp.sec.home.tld' inherits basenode {
 	
 	# Note: requires a copy of hosts 'fstab' file at puppetmaster.
     class { admin_fstab::config : fstabhost => 'warp' }
 
 
-    # assumes that all host lives in the same domain (for now..)
+    # if this host is not in the same domain as servers, specify it as a parameter
     class { admin_hosts::config :
-        puppetserver_ip => '192.168.0.24', puppetserver_hostname => 'carbon',
-        gateway_ip => '192.168.0.1', gateway_hostname => 'gondor',
-        smtp_ip => '192.168.0.11', smtp_hostname => 'rohan' }
+        puppetserver_ip => '192.168.0.24', puppetserver_hostname => 'carbon', puppetserver_domain => 'home.tld',
+        gateway_ip => '192.168.0.1', gateway_hostname => 'gondor', gateway_domain => 'home.tld',
+        smtp_ip => '192.168.0.11', smtp_hostname => 'rohan', smtp_domain => 'home.tld' }
     
 	## network and default services
 	
-    class { puppet_network::interfaces : broadcastnet => '192.168.0.0', defaultgateway => '192.168.0.1' }
+    class { puppet_network::interfaces : broadcastnet => '192.168.2.0', defaultgateway => '192.168.2.1' }
 	
     class { puppet_iptables::config : role => 'fileserver', hostnm => 'warp' }
 	
