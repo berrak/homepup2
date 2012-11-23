35f6c7896845dd0863ca587ed1345b8fc7c99832
berrak <bkronmailbox-git@yahoo.se>
2012-11-23 10:16:02 +0100

    Fix: Duplicate declaration of resource name.

--- manifests/gateways.pp
+++ manifests/gateways.pp
@@ -14,11 +14,11 @@ node 'gondor.home.tld' inherits basenode {
     include admin_hardening
 		
 	# run tripwire checks 3 times per day and have tripwire mail to root (but not cron daemon)
-    admin_cron::install { 'tripwire' :
+    admin_cron::install { 'tripwire13' :
 	    command => '/root/bin/tripwire.check > /dev/null 2>&1', hour => '13', minute => '0' }
-    admin_cron::install { 'tripwire' :
+    admin_cron::install { 'tripwire21' :
 	    command => '/root/bin/tripwire.check > /dev/null 2>&1', hour => '21', minute => '0' }
-    admin_cron::install { 'tripwire' :
+    admin_cron::install { 'tripwire05' :
 	    command => '/root/bin/tripwire.check > /dev/null 2>&1', hour => '5', minute => '0' }
 	
     # Note: requires a copy of hosts 'fstab' file at puppetmaster.
