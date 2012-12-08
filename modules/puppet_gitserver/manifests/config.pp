13ea7aadf74ea1cc16c162869695f559ceef566e
berrak <bkronmailbox-git@yahoo.se>
2012-12-08 19:02:58 +0100

    More understandable resource names.

--- modules/puppet_gitserver/manifests/config.pp
+++ modules/puppet_gitserver/manifests/config.pp
@@ -22,48 +22,46 @@ define puppet_gitserver::config ( $gitgrp ='', $projectname = '' ) {
         fail("FAIL: No git group name ($gitgrp) given as parameter.")
     }
 	
-	# create the git depot for the developers
-	# in group '$gitgrp' if it does not exist
+	## create the git depot for the developers
+	## in group '$gitgrp' if not existing.
 	
-	exec { "Create_depot_${name}":
+	exec { "Create_Depot_Group_${name}":
 	   command => "/bin/mkdir /srv/${gitgrp}",
 	    onlyif => "/usr/bin/test ! -d /srv/${gitgrp}",
 	}
 	
 	exec { "Chown_${name}":
 	    command => "/bin/chown root:${gitgrp} /srv/${gitgrp}",
-	      subscribe => Exec["Create_depot_${name}"],
+	      subscribe => Exec["Create_Depot_Group_${name}"],
 	    refreshonly => true,
 	}
 	
 	exec { "Chmod_${name}":
 	    command => "/bin/chmod 0750 /srv/${gitgrp}",
-	      subscribe => Exec["Create_depot_${name}"],
+	      subscribe => Exec["Create_Depot_Group_${name}"],
 	    refreshonly => true,
 	}
 	
-	## create the project (depends on that the git group is created)
+	## create the project directory if not already existing.
 	
-	exec { "Create_project_${name}":
+	exec { "Create_Directory_${name}":
 	        command => "/bin/mkdir /srv/${gitgrp}/${projectname}",
 	    onlyif => "/usr/bin/test ! -d /srv/${gitgrp}/${projectname}",
 	}
 	
-	exec { "Initilize_git_depot_${name}":
+	exec { "Initilize_Depot_${name}":
 	         cwd => "/srv/${gitgrp}/${projectname}",
 	        command => "/usr/bin/git init --bare",
-	      subscribe => Exec["Create_project_${name}"],
+	      subscribe => Exec["Create_Directory_${name}"],
 	    refreshonly => true,
 	}
 	
 	## finally set the ownerships to the new project to the git group
 	
-	exec { "Chown_git_project_${name}":
+	exec { "Recursive_Chown_${name}":
 	    command => "/bin/chown -R ${gitgrp}:${gitgrp} /srv/${gitgrp}/${projectname}",
-	      subscribe => Exec["Initilize_git_depot_${name}"],
+	      subscribe => Exec["Initilize_Depot_${name}"],
 	    refreshonly => true,
 	}
 	
-	
-    
 }
