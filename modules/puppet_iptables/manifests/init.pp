86ceaa1b96891754273b5b66ddc16e398e713009
berrak <bkronmailbox-git@yahoo.se>
2013-06-20 11:58:53 +0200

    Fix: forgot to declare params manifest

--- modules/puppet_iptables/manifests/init.pp
+++ modules/puppet_iptables/manifests/init.pp
@@ -3,6 +3,6 @@
 ##
 class puppet_iptables inherits root_home {
 
-    include puppet_iptables::install, puppet_iptables::config
+    include puppet_iptables::install, puppet_iptables::config, puppet_iptables::params
 
 }
