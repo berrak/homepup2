d96de0da40114852093448265ca12c1ef6753305
berrak <bkronmailbox-git@yahoo.se>
2013-08-16 19:25:08 +0200

    Add pythons document generator

--- modules/admin_bndl/manifests/install.pp
+++ modules/admin_bndl/manifests/install.pp
@@ -148,9 +148,10 @@ define admin_bndl::install {
         pythonapps : {
         
             # pyflakes: passive checker of Python 2 and 3 programs
-			# python-pyside: python bindings/interface to qt4 
+			# python-pyside: python bindings/interface to qt4
+            # python-sphinx, sphinx-doc: python documentation generator
           
-	        package  { [ "python-pyside", "pyflakes"]:
+	        package  { [ "python-pyside", "pyflakes", "python-sphinx", "sphinx-doc" ]:
                  ensure => installed }
                  
         }
