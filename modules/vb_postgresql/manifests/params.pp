792650be2a8ffb680d11af09b70c05af8835316a
berrak <bkronmailbox-git@yahoo.se>
2013-12-28 23:49:47 +0100

    Change to ASCII to work with OC ESQL and sv_SE

--- modules/vb_postgresql/manifests/params.pp
+++ modules/vb_postgresql/manifests/params.pp
@@ -4,10 +4,12 @@
 class vb_postgresql::params {
 
     # define western Europe encoding for a new database, ISO8859-1 (LATIN) is fine for sweden
-    
-    $database_encoding = "ENCODING='LATIN1' LC_COLLATE='sv_SE' LC_CTYPE='sv_SE' "
+    # $database_encoding = "ENCODING='LATIN1' LC_COLLATE='sv_SE' LC_CTYPE='sv_SE' "
     
     # UTF-8 does not work well with OpenCobol ESQL library 1.0.0
     # $database_encoding = "ENCODING='UTF8' LC_COLLATE='sv_SE.utf8' LC_CTYPE='sv_SE.utf8' "
+    
+    # Only ASCII works for OC ESQL, which is default in database, thus an empty string here
+    $database_encoding =''
 
 }
