4704658020dfb6c1dae735711887a9f9386927c0
berrak <bkronmailbox-git@yahoo.se>
2013-12-28 23:13:19 +0100

    Set encoding to ISO8859-1

--- modules/vb_postgresql/manifests/params.pp
+++ modules/vb_postgresql/manifests/params.pp
@@ -3,9 +3,9 @@
 #
 class vb_postgresql::params {
 
-    # define the encoding scheme for a new database, ISO8859-15 (LATIN9) is fine for sweden
+    # define western Europe encoding for a new database, ISO8859-1 (LATIN) is fine for sweden
     
-    $database_encoding = "ENCODING='LATIN9' LC_COLLATE='sv_SE.885915' LC_CTYPE='sv_SE.885915' "
+    $database_encoding = "ENCODING='LATIN1' LC_COLLATE='sv_SE' LC_CTYPE='sv_SE' "
     
     # UTF-8 does not work well with OpenCobol ESQL library 1.0.0
     # $database_encoding = "ENCODING='UTF8' LC_COLLATE='sv_SE.utf8' LC_CTYPE='sv_SE.utf8' "
