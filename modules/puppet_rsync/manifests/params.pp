ebdba0b91ddee8c0a20db9eda3aaf5be5df8a9a0
berrak <bkronmailbox-git@yahoo.se>
2014-01-01 19:26:01 +0100

    Change backup of source to home directory

--- modules/puppet_rsync/manifests/params.pp
+++ modules/puppet_rsync/manifests/params.pp
@@ -22,6 +22,6 @@ class puppet_rsync::params {
     
     # list of client hosts that backup with rsync
     
-    $clienthostlist = [ 'warp', 'shire', 'mordor' ]
+    $clienthostlist = [ 'warp', 'shire', 'mordor', dell ]
     
 }
