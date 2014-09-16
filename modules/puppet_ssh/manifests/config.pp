94c95c5f650737385635bc75565b60c4d3f6638b
berrak <bkronmailbox-git@yahoo.se>
2014-09-16 18:14:30 +0200

    Add banner to ssh login

--- modules/puppet_ssh/manifests/config.pp
+++ modules/puppet_ssh/manifests/config.pp
@@ -3,23 +3,38 @@
 ##
 class puppet_ssh::config {
     
-        file { "/root/.ssh":
-            ensure => "directory",
-             owner => 'root',
-             group => 'root',
-              mode => '0700',
-	    }
-        
-        #  This script will append the public host key (id_rsa.pub) to
-        #  this host and user '~/.ssh/authorized_keys' file. This helper
-        #  is used by 'ssh.cpkey' script when a remote clients sends
-        #  its 'id_rsa.pub' file and is therfore automaticall invoked. 
-        
-        file { "/root/bin/ssh.addkey" :
-                source => "puppet:///modules/puppet_ssh/ssh.addkey",
-                 owner => 'root',
-                 group => 'root',
-                  mode => '0700',
-        }	 
+	file { "/root/.ssh":
+		ensure => "directory",
+		 owner => 'root',
+		 group => 'root',
+		  mode => '0700',
+	}
+	
+	#  This script will append the public host key (id_rsa.pub) to
+	#  this host and user '~/.ssh/authorized_keys' file. This helper
+	#  is used by 'ssh.cpkey' script when a remote clients sends
+	#  its 'id_rsa.pub' file and is therfore automaticall invoked. 
+	
+	file { "/root/bin/ssh.addkey" :
+			source => "puppet:///modules/puppet_ssh/ssh.addkey",
+			 owner => 'root',
+			 group => 'root',
+			  mode => '0700',
+	}	 
+
+	file { "/etc/ssh/sshd_config" :
+			source => "puppet:///modules/puppet_ssh/sshd_config",
+			 owner => 'root',
+			 group => 'root',
+			  mode => '0644',
+			notify => Service["ssh"],
+	}	 
+
+	file { "/etc/issue.net" :
+			source => "puppet:///modules/puppet_ssh/issue.net",
+			 owner => 'root',
+			 group => 'root',
+			  mode => '0644',
+	}	 
 
 }
