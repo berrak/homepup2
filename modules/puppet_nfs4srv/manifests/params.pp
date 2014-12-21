e176c316d29a82e99ee673e5f36a1e479317d4cd
berrak <bkronmailbox-git@yahoo.se>
2014-12-21 17:05:00 +0100

    Add hp test server node

--- modules/puppet_nfs4srv/manifests/params.pp
+++ modules/puppet_nfs4srv/manifests/params.pp
@@ -13,5 +13,6 @@ class puppet_nfs4srv::params {
     $export1 = '/exports/usernfs4/bekr 192.168.0.100/24(rw,no_subtree_check)'
     $export2 = '/exports/usernfs4/bekr 192.168.0.222/24(rw,no_subtree_check)'
     $export3 = '/exports/usernfs4/bekr 192.168.0.24/24(rw,no_subtree_check)'  
+    $export4 = '/exports/usernfs4/bekr 192.168.0.66/24(rw,no_subtree_check)'
     
 }
