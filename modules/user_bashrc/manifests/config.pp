c6e2da61953889add558662c97e510fa83a0f641
berrak <bkronmailbox-git@yahoo.se>
2015-01-04 12:37:44 +0100

    Add missing user in check

--- modules/user_bashrc/manifests/config.pp
+++ modules/user_bashrc/manifests/config.pp
@@ -15,7 +15,7 @@ define user_bashrc::config {
 
     # array of real users...(not root, or system accounts)
 		
-    if ( $name in ["bekr", "dakr", "levonline", "jensen"] ) {
+    if ( $name in ["bekr", "dakr", "levonline", "jensen", "bkron"] ) {
 		
 		# create a couple of standard sub directories for the user
 		
